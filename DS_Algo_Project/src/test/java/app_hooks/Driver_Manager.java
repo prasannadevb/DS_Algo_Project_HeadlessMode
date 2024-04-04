4d61916d11be6a97414e881d40148cf0448d60be
bindu <bindu@prabhaasus>
2024-04-04 08:54:02 -0400

    headless mode change in driver manager

--- DS_Algo_Project/src/test/java/app_hooks/Driver_Manager.java
+++ DS_Algo_Project/src/test/java/app_hooks/Driver_Manager.java
@@ -7,9 +7,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.ITestContext;
 import org.testng.annotations.BeforeMethod;
 
@@ -22,6 +25,7 @@ public class Driver_Manager {
 	private static Driver_Manager driverManager;
 
 	private static final ThreadLocal<WebDriver> drivers = new ThreadLocal<>();
+	private static final ChromeDriverService options = null;
 	private Driver_Manager() {};
 
 	public static Driver_Manager getInstance() {
@@ -35,18 +39,24 @@ public class Driver_Manager {
 	public  WebDriver createDriver(String browser) {
 		// TODO Auto-generated method stub
 
+		
 		try {
 
 			switch (browser) {
 			case "chrome":
 
 				LOGGER.info("Launching "+ Constants.BROWSER);
-				return new ChromeDriver();
-
-			case "firefox":
+				ChromeOptions options = new ChromeOptions();
+				 options.addArguments("headless");
+				 return new ChromeDriver(options);
+				
+				
+				
+            case "firefox":
 
 				LOGGER.info("Launching "+ Constants.BROWSER);
 				return  new FirefoxDriver();
+				
 
 			case "ie":
 
